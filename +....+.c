e9314b5d47a74be0f9c4c9c7c8f38a1c0763067f
Mari-Chiara <127746101+Mari-Chiara@users.noreply.github.com>
2023-03-13 15:45:35 +0100

    Add files via upload

---
+++ +....+.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int numero = 4;
+    double numerolungo = 1000000000; 
+    float numeroconlavirgola = 20,5;
+    char carattere = 'c';
+    
+    printf("il numero è: %d\n", numero); 
+    printf("il numero è: %f, numero con la virgola è:%f\n", numerolungo, numeroconlavirgola);
+    printf ("il carattere è: %c", carattere);
+    }
